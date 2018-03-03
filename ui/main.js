acc5d21f78345c6c0a5cba6c32596292d588b96c
srilakshmii <610sssg@gmail.com>
2018-03-03 12:54:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,4 @@ var element=document.getElementById('a');
 
 element.innerHTML='okay';
 
-var img=document.getElementById('b');
-var x=document.createElement("ima");
-x.setAttribute("src","https://fyf.tac-cdn.net/images/products/large/BF116-11KM.jpg");
-img.innerHTML=x;
+
