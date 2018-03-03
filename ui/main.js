dbd94007fd831b47f43c2ff2a0fa3e6fce329c8f
srilakshmii <610sssg@gmail.com>
2018-03-03 13:15:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,11 @@
-console.log('Loaded!');
-
-
+var a=document.getElementById("counter");
+var counter=0;
+a.onclick =function()
+{
+   counter=counter+1;
+   var b=document.getElementById("count");
+   b.innerHTML=counter.toString();
+   
+}
 
 
