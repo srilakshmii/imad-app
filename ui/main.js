cd984f70ab55fd5e3aa1b81b0bdf582f09e55f1f
srilakshmii <610sssg@gmail.com>
2018-03-04 18:51:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,20 +23,36 @@ a.onclick =function()
 };
 
 
-var nme=document.getElementById('name');
-var name=nme.value;
+
 var submit=document.getElementById('submit_btn');
 submit.onclick=function()
 {
-   var names=['var1','var2','var3'];
-   var list='';
-   for(var i=0;i<names.length;i++)
-   {
-       list+='<li>'+names[i]+'</li>';
-       
+     var request=new XMLHttpRequest();
+   request.onreadystatechange=function(){
+       if (request.readyState== XMLHttpRequest.DONE)
+       {
+           if(request.status==200)
+           {
+               var nme=document.getElementById('name');
+                var name=nme.value;
+                var names=['var1','var2','var3'];
+               var list='';
+               for(var i=0;i<names.length;i++)
+               {
+                   list+='<li>'+names[i]+'</li>';
+                   
+               }
+               var ul=document.getElementById('ul');
+               ul.innerHTML=list;
+            }
+        }
    }
-   var ul=document.getElementById('ul');
-   ul.innerHTML=list;
+   request.open('GET','http://u610sssg.imad.hasura-app.io/submit-name?name='+name,true);
+   request.send(null);
+   
+   
+   
+  
    
     
 };
